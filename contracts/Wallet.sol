1ed09472f39e66047ae6098835fca5477e1c0736
skiran017 <skiran017@gmail.com>
2022-01-22 22:04:32 +0530

    Smart Contract, Migration and Test

---
+++ contracts/Wallet.sol
@@ -0,0 +1,72 @@
+pragma solidity 0.6.0;
+pragma experimental ABIEncoderV2;
+
+contract Wallet {
+    address[] public approvers; // addresses of total no. of approvers
+    uint256 public quorum; //no. of apporvers needed to approve the transaction
+    struct Transfer {
+        uint256 id;
+        uint256 amount;
+        address payable to;
+        uint256 approvals;
+        bool sent;
+    }
+
+    Transfer[] public transfers;
+    mapping(address => mapping(uint256 => bool)) public approvals;
+
+    constructor(address[] memory _approvers, uint256 _quorum) public {
+        approvers = _approvers;
+        quorum = _quorum;
+    }
+
+    //function to get list of approvers
+    function getApprovers() external view returns (address[] memory) {
+        return approvers;
+    }
+
+    function getTransfers() external view returns (Transfer[] memory) {
+        return transfers;
+    }
+
+    //function to create transfers which will be called by one of the approvers
+    function createTransfer(uint256 amount, address payable to)
+        external
+        onlyApprover
+    {
+        transfers.push(Transfer(transfers.length, amount, to, 0, false));
+    }
+
+    function approveTransfer(uint256 id) external onlyApprover {
+        require(transfers[id].sent == false, "transfer has already been sent");
+        require(
+            approvals[msg.sender][id] == false,
+            "cannot approve transfer twice"
+        );
+
+        approvals[msg.sender][id] = true;
+        transfers[id].approvals++;
+
+        if (transfers[id].approvals >= quorum) {
+            transfers[id].sent = true;
+            address payable to = transfers[id].to;
+            uint256 amount = transfers[id].amount;
+            to.transfer(amount);
+        }
+    }
+
+    //function to receive ether (for SC)
+    receive() external payable {}
+
+    //to have access control we declare a modifier
+    modifier onlyApprover() {
+        bool allowed = false;
+        for (uint256 i = 0; i < approvers.length; i++) {
+            if (approvers[i] == msg.sender) {
+                allowed = true;
+            }
+        }
+        require(allowed == true, "Only approver allowed");
+        _;
+    }
+}
